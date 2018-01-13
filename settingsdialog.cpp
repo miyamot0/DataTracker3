6a837af4a9d49db45bf013099668b76566a25f67
Shawn Gilroy <shawn.gilroy@temple.edu>
2018-01-13 14:09:53 +0000

    Fix atypical location issues

--- settingsdialog.cpp
+++ settingsdialog.cpp
@@ -221,6 +221,13 @@ void SettingsDialog::on_setSaveLocation_clicked()
                                                        tr("Pick Alternate Save Location"),
                                                        QStandardPaths::standardLocations(QStandardPaths::DocumentsLocation)[0],
                                                        QFileDialog::ShowDirsOnly);
+
+
+        if (mLocation.contains("DataTracker3", Qt::CaseInsensitive))
+        {
+            mLocation = mLocation.replace("DataTracker3", "");
+        }
+
 #elif TARGET_OS_MAC
         mLocation = QFileDialog::getExistingDirectory (this,
                                                        tr("Pick Alternate Save Location"),
@@ -235,6 +242,12 @@ void SettingsDialog::on_setSaveLocation_clicked()
                                                        tr("Pick Alternate Save Location"),
                                                        ui->editSaveLocation->text(),
                                                        QFileDialog::ShowDirsOnly);
+
+        if (mLocation.contains("DataTracker3", Qt::CaseInsensitive))
+        {
+            mLocation = mLocation.replace("DataTracker3", "");
+        }
+
 #elif TARGET_OS_MAC
         mLocation = QFileDialog::getExistingDirectory (this,
                                                        tr("Pick Alternate Save Location"),
